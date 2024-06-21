a403edcf0e55969b8273869e6a9095342cb571ec
Oleksandr Kryvonos <geraclion@gmail.com>
2024-06-21 23:08:19 +0200

    simplification of code

--- function-node.gd
+++ function-node.gd
@@ -9,7 +9,9 @@ var file_name_1: String = "AddInformationToExistingOne.js"
 func combined_path():
 	return folder_path + file_name_1
 	
-func populate_new_function(name: String):
+func populate_new_function(start_index, end_index):
+	var name = text.substr( start_index, end_index - start_index + 1)
+	print(name)
 	self.get_parent().populate_new_function(name, max_x + position.x, position.y)
 
 var max_x = 0
@@ -17,48 +19,34 @@ var max_y = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var file = FileAccess.open(combined_path(), FileAccess.READ)
+	file.get
 	var content = file.get_as_text()
-	var splitted = content.split(" ",false)
-	#(get_node("name") as RichTextLabel).text = content
 	text = content
-	if false:
-		print(content)
-	var position = Vector2(0, 0)
-	var previous_symbol = null
-	var index = 0
-	for x in content:
-		var clone = symbol_scene.instantiate()
-		clone._previous_symbol = previous_symbol
-		clone.index = index
-		clone.function_parent = self
-		clone.set_text(x)
-		
-		previous_symbol = clone
-		var size = clone.get_size()
-		
 
-		if x == '\n':
-			position.y += size.y
-			position.x = 0
-		
-		if position.x > max_x:
-			max_x = position.x
-		
-		if position.y > max_y:
-			max_y = position.y
-
-		clone.position = position
-		position.x += size.x
-		
-		if false:
-			print(clone.get_size())
-		add_child(clone)
-		index += 1
-		
-
-	pass # Replace with function body.
-
+var index = -1
+var _position = Vector2(0, 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if index < text.length():
+		index += 1
+		var x = text.substr(index, 1)
+		var clone = symbol_scene.instantiate()
+		add_child(clone)
+		clone.set_text(x)
+		
+		var size = clone.get_size()
+		if x == '\n':
+			_position.y += size.y
+			_position.x = 0
+		
+		if _position.x > max_x:
+			max_x = _position.x
+		
+		if _position.y > max_y:
+			max_y = _position.y
+
+		clone.position = _position
+		_position.x += size.x
+		
+		
