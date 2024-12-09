994e95ee50bdee8f86fc350152c9c56a23c0139c
Oleksandr Kryvonos <geraclion@gmail.com>
2024-12-09 23:04:10 +0100

    2024-12-09--23h04m10s

--- scenes/caret.gd
+++ scenes/caret.gd
@@ -69,4 +69,6 @@ func _on_rocketengine_request_to_shift_position(shift):
 
 func _on_rocketengine_request_to_set_position(another_position):
 	position = another_position
-	
+	var next = desired_locations.pop_front()
+	if next is null:
+		$"Rocket-engine".update_position()
